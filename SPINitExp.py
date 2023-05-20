a55577d6d4f5882e6e27f73254264e5e1a73129a
Xiao JI <xiao.ji@ucsf.edu>
2023-05-19 17:53:46 -0700

    Changes to be committed: 	new file:   .gitignore 	modified:   SPINitExp.py

---
+++ SPINitExp.py
@@ -0,0 +1,31 @@
+# Official packages
+import os
+import copy
+
+from typing import List, Dict
+
+# Third-party packages
+import numpy as np
+
+# In-house packages
+
+class SPINitExp(object):
+    """
+        Basic Class that that read, parses, and stores parameters and data acquired from the SPINit-SpinAligner platform.
+
+        Parameters:
+        -----------
+
+        exp_data_path: str
+            experimental data path
+
+    """
+    
+    def __init__(self, exp_dataset_path:str, **kwargs) -> None:
+        
+        """
+        
+        """
+        self.params = dict()
+        self.data = np.array([])
+        
