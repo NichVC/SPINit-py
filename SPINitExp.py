fd6820e550564d9126c4efd87f69777c91a0cdc4
X.JI@94107 <xiao.ji@ucsf.edu>
2023-05-20 01:43:21 -0700

    Changes to be committed:     modified:   SPINitExp.py     modified:   testSPINit-py.ipynb

--- SPINitExp.py
+++ SPINitExp.py
@@ -20,6 +20,15 @@ class SPINitExp(object):
             experimental data path
 
     """
+
+    FILE_PATH_TEMPLATE = {
+        "data"  = "data.dat",
+        "Serie" = "Serie.xml",
+        "header"= "header.xml"
+    }
+
+
+
     
     def __init__(self, exp_dataset_path:str, **kwargs) -> None:
         
@@ -29,7 +38,21 @@ class SPINitExp(object):
             i.  data.dat    : binary data file
             ii. Serie.xml   : parameter file
             iii.header.xml  : parameter file
-
+        1. load parameters from files
+        2. load data from file
         
         """
-        
+        self._file_paths = self._verify_dataset(exp_dataset_path)
+
+        self.params = self._load_params()
+        self.data = self._load_data()
+
+    def _verify_dataset(self, exp_dataset_path):
+        return NotImplemented
+
+    def _load_params(self):
+        return NotImplemented
+
+    def _load_data(self):
+        return NotImplemented
+
